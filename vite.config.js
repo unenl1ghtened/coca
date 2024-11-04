8098163cb9522c0d2b57337dfe880d2691218932
Margarita <margarita0rzkv@gmail.com>
2024-11-04 19:49:17 +0000

    Work with html and images

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,40 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
+        api: 'modern-compiler',
       },
     },
   },
-  root: "src",
+  root: './src',
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
+    },
   },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
