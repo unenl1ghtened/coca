6047a8e13aa75a870713439bfe9b8ceaf023f581
Margarita <margarita0rzkv@gmail.com>
2024-11-07 13:37:16 +0000

    Fixed header scrolling and overlay z-index to 3

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -3,17 +3,20 @@ export const useBurger = () => {
   const burgerBtn = document.querySelector('[data-burger="btn"]');
   const overlay = document.querySelector('[data-burger="overlay"]');
   const burgerMenu = document.querySelector('[data-burger="menu"]');
+  const body = document.body;
 
   const closeBurgerMenu = () => {
     burgerBtn.classList.remove('burger--active');
     overlay.classList.remove('overlay--visible');
     burgerMenu.classList.remove('header__right--visible');
+    body.classList.remove('body--fixed');
   };
 
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger--active');
     overlay.classList.toggle('overlay--visible');
     burgerMenu.classList.toggle('header__right--visible');
+    body.classList.toggl('body--fixed');
   });
 
   overlay.addEventListener('click', closeBurgerMenu);
