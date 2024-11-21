11af1e024d65ab6ff404d67b2f41c98e48af51d2
Margarita <margarita0rzkv@gmail.com>
2024-11-21 18:37:10 +0000

    Created contact get-started section

---
+++ src/js/components/contact/phone.js
@@ -0,0 +1,16 @@
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
+
+export const usePhone = () => {
+  const input = document.querySelector('#phone');
+  intlTelInput(input, {
+    initialCountry: 'auto',
+    geoIpLookup: (callback) => {
+      fetch('https://ipapi.co/json')
+        .then((res) => res.json())
+        .then((data) => callback(data.country_code))
+        .catch(() => callback('us'));
+    },
+    utilsScript: `https://cdn.jsdelivr.net/npm/intl-tel-input@${intlTelInput.version}/build/js/utils.js`,
+  });
+};
--- src/js/contact.js
+++ src/js/contact.js
@@ -5,9 +5,11 @@ import { useBurger } from './components/burger.js';
 import { useHeader } from './components/scroll.js';
 import { useFooterAccordion } from './components/footer-accordion.js';
 import { usePartnersSlider } from './components/home/slider.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useBurger();
 useHeader();
 usePartnersSlider();
 useFooterAccordion();
+usePhone();
