1d99997c6462f33f645dfd066f90ebb6a3f1b639
Margarita <margarita0rzkv@gmail.com>
2024-11-28 18:57:46 +0000

    Created blog articles section

--- src/js/blog.js
+++ src/js/blog.js
@@ -7,6 +7,7 @@ import { useBurger } from './components/burger.js';
 import { useHeader } from './components/scroll.js';
 import { useFooterAccordion } from './components/footer-accordion.js';
 import { useHeroSlider, useTabControl } from './components/blog/heroSlider.js';
+import { useArticlesSlider } from './components/blog/articlesSlider.js';
 
 useHeader();
 useTheme();
@@ -14,3 +15,4 @@ useBurger();
 useFooterAccordion();
 useHeroSlider();
 useTabControl();
+useArticlesSlider();
